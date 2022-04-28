1cdcda08ff7b5a5d1d93350fd7115a1a1ce227eb
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-04-28 14:18:57 -0300

    Função horas

--- Projeto contador/contador.js
+++ Projeto contador/contador.js
@@ -6,18 +6,29 @@ function contadores() {
     let elemento = document.querySelector('span#contador');
     let radio = document.getElementsByName('contadorRadio');
     if (radio[0].checked) {
-        id = setInterval(() => elemento.innerHTML = contador++, 100);
+        id = setInterval(() => elemento.innerHTML = contador++, 1000);
     } else {
-        id = setInterval(() => elemento.innerHTML = contadorRegre--, 100);
+        id = setInterval(() => elemento.innerHTML = contadorRegre--, 1000);
     }
 }
 function pararContagem() {
     clearInterval(id);
 }
 function recomecar() {
-    contadorRegre = 0;
+    contadorRegre = 1000;
     contador = 0;
 }
+function horario () {
+    let resp = document.querySelector('span#horas');
+
+    let data = new Date ();
+    let hora = data.getHours;
+    let minutos = data.getMilliseconds;
+    let segundos = data.getSeconds;
+
+    resp.innerHTML =` ${hora}:${minutos}:${segundos}`
+}
+
 
 
 
