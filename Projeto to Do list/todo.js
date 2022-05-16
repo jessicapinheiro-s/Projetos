424568443ea2c82749517b4d23526d785a1a1250
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-16 09:35:32 -0300

    Subindo projeto to Do list
    Projeto ainda não finalizado

---
+++ Projeto to Do list/todo.js
@@ -0,0 +1,27 @@
+let inTarefa = document.getElementById('inTarefa');
+let tarefa = inTarefa.value;
+function addTarefa (){
+    let resp = document.querySelector('div.resposta');
+    if(tarefa == ''){
+        resp.innerHTML = `Preencha o campo corretamente ..`;
+        inTarefa.focus();
+    }else{
+        let ul = document.getElementById('lista');
+        let li = document.createElement('li');
+        let txt = document.createTextNode(tarefa);
+        let btExcluir = document.createElement('button');
+        let txtBt = document.createTextNode('Excluir');
+        btExcluir.id = 'btExcluir';
+    
+        li.appendChild(txt);
+        ul.appendChild(li);
+        li.appendChild(btExcluir);
+        btExcluir.appendChild(txtBt);
+        inTarefa.value = '';
+        inTarefa.focus();
+    }
+
+}
+function excluir () {
+    tarefa.remove();
+}
