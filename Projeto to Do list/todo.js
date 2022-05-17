f26b08f8e4284de188bdcb2aa25c520562e66d9e
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-17 12:55:30 -0300

    adicionando a checkbox

--- Projeto to Do list/todo.js
+++ Projeto to Do list/todo.js
@@ -1,28 +1,41 @@
-let inTarefa = document.getElementById('inTarefa');
-let tarefa = inTarefa.value;
+
 
 function addTarefa (){
+    let inTarefa = document.getElementById('inTarefa');
+    let tarefa = inTarefa.value;
     let resp = document.querySelector('div.resposta');
-    
-    if(tarefa == ' '){
-        resp.innerHTML = `Preencha o campo corretamente ..`;
-        inTarefa.focus();
-    }else{
-        let ul = document.getElementById('lista');
-        let li = document.createElement('li');
-        let txt = document.createTextNode(tarefa);
-        let btExcluir = document.createElement('button');
-        let txtBt = document.createTextNode('Excluir');
-        
-        btExcluir.id = 'btExcluir';
-    
-        li.appendChild(txt);
-        ul.appendChild(li);
-        li.appendChild(btExcluir);
-        btExcluir.appendChild(txtBt);
+        if( tarefa == ''){
+            resp.innerHTML = `O campo está vazio, digite algo`
+            inTarefa.focus();
+            return;
+        }else{
+            resp. innerHTML = '';
+            let lista = document.getElementById('lista');
+            let li = document.createElement('li');
+            let txt = document.createTextNode(tarefa);
+            let btExcluir = document.createElement('button');
+            let txtBt = document.createTextNode('Excluir');
+            
+            let checkbox = document.createElement('input');
+            checkbox.classList.add('checkbox');
+            checkbox.type = 'checkbox';
+            checkbox.id = 'checkbox';
 
-        inTarefa.value = '';
-        inTarefa.focus();
-    }
 
+            btExcluir.id = 'btExcluir';
+
+
+            li.appendChild(checkbox);
+            li.appendChild(txt);
+            lista.appendChild(li);
+            li.appendChild(btExcluir);
+            btExcluir.appendChild(txtBt);
+    
+            inTarefa.value = '';
+            inTarefa.focus();
+        }
+
+}
+function excluir () {
+    tarefa.remove();
 }
