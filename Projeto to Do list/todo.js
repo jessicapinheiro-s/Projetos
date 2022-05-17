ca8d3869d42fad6126d5ed9cf3b6f2767edde70f
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-17 14:31:43 -0300

    editando a lista li

--- Projeto to Do list/todo.js
+++ Projeto to Do list/todo.js
@@ -1,41 +1,64 @@
 
 
-function addTarefa (){
+function addTarefa() {
     let inTarefa = document.getElementById('inTarefa');
     let tarefa = inTarefa.value;
     let resp = document.querySelector('div.resposta');
-        if( tarefa == ''){
-            resp.innerHTML = `O campo está vazio, digite algo`
-            inTarefa.focus();
-            return;
-        }else{
-            resp. innerHTML = '';
-            let lista = document.getElementById('lista');
-            let li = document.createElement('li');
-            let txt = document.createTextNode(tarefa);
-            let btExcluir = document.createElement('button');
-            let txtBt = document.createTextNode('Excluir');
-            
-            let checkbox = document.createElement('input');
-            checkbox.classList.add('checkbox');
-            checkbox.type = 'checkbox';
-            checkbox.id = 'checkbox';
 
-
-            btExcluir.id = 'btExcluir';
-
-
-            li.appendChild(checkbox);
-            li.appendChild(txt);
-            lista.appendChild(li);
-            li.appendChild(btExcluir);
-            btExcluir.appendChild(txtBt);
+    while (tarefa == ''){
+        resp.innerHTML = `O campo está vazio, digite algo`
+        inTarefa.focus();
+        return;
+    }
+  
+    resp.innerHTML = '';
     
-            inTarefa.value = '';
-            inTarefa.focus();
+    let lista = document.getElementById('lista');
+    let li = document.createElement('li');
+    let txt = document.createTextNode(tarefa);
+    let btExcluir = document.createElement('button');
+    let txtBt = document.createTextNode('Excluir');
+
+    
+    let checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkbox';
+
+
+    btExcluir.id = 'btExcluir';
+
+
+    li.appendChild(checkbox);
+    li.appendChild(txt);
+    lista.appendChild(li);
+    li.appendChild(btExcluir);
+    btExcluir.appendChild(txtBt);
+
+    inTarefa.value = '';
+    inTarefa.focus();
+}
+
+
+function excluir() {
+    let checkbox = document.getElementsByTagName('input');
+    let lista = document.getElementById('lista');
+    let li = document.getElementsByTagName('li');
+    
+    if(checkbox[0].checked){
+        if(lista.parentNode){
+            lista.parentNode.removeChild(lista);
         }
+    }
+    
+   
+    let rem = lista.removeChild(li);
+
+
+    
 
 }
-function excluir () {
-    tarefa.remove();
-}
+let btExcluir = document.getElementById('btExcluir');
+btExcluir.addEventListener('click', excluir);
+
+
