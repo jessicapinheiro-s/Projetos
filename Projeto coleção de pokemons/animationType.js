ec3f6c26e85bdb6717bbd55ad222d23c3c0db8b9
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-18 12:16:37 -0300

    criando estrutura dos cards

---
+++ Projeto coleção de pokemons/animationType.js
@@ -0,0 +1,5 @@
+const typeWriter = document.getElementById('typewriter-text');
+const text = 'Bem-vindos(as) a minha coleção.';
+
+typeWriter.innerHTML = text;
+typeWriter.style.setProperty('--characters', text.length);
