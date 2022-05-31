1bab6d50e65446549177ae826e1c36cd549ebc8c
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-31 13:35:54 -0300

    criando funções

---
+++ Projeto calculadora IMC/app.js
@@ -0,0 +1,11 @@
+function calcularPeso (){
+
+}
+let btCalcular = document.getElementById('btCalcular');
+btCalcular.addEventListener('click', calcularPeso);
+
+function limpar (){
+
+}
+let btLimpar = document.getElementById('btLimpar');
+btLimpar.addEventListener('click', calcularPeso);
