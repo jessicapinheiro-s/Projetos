4ed4f9eb475affb4c06b6ddab10324aad0b646fc
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-05-31 14:24:40 -0300

    arrumando função limpar

--- Projeto calculadora IMC/app.js
+++ Projeto calculadora IMC/app.js
@@ -1,11 +1,30 @@
-function calcularPeso (){
+let inAltura = document.getElementById('inAltura');
+let inPeso = document.getElementById('inPeso');
 
+let resp = document.querySelector('div.resposta');
+
+function calcularPeso (){
+    let altura = Number(inAltura.value);
+    let peso = Number(inPeso.value);
+
+    let calculoIMC = peso / (altura * altura); 
+    resp.innerHTML = `${calculoIMC}`;
 }
 let btCalcular = document.getElementById('btCalcular');
 btCalcular.addEventListener('click', calcularPeso);
 
 function limpar (){
+    let altura = Number(inAltura.value);
+    let peso = Number(inPeso.value);
 
+    resp.innerHTML = '';
+
+    if(altura == '' || peso == ''){
+        resp.innerHTML = `Os campos já estão vazios ..`
+    }else{
+        inAltura.value = ' ';
+        inPeso.value = ' '; 
+    }
 }
 let btLimpar = document.getElementById('btLimpar');
-btLimpar.addEventListener('click', calcularPeso);
+btLimpar.addEventListener('click', limpar);
