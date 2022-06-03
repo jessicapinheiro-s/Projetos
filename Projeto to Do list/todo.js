9bec486acfa5ca147becc78edacb1c7a306dd4ec
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-06-03 16:28:51 -0300

    adicionando function excluir

--- Projeto to Do list/todo.js
+++ Projeto to Do list/todo.js
@@ -1,20 +1,20 @@
-
 function addTarefa() {
     let inTarefa = document.getElementById('inTarefa');
     let tarefa = inTarefa.value;
     let resp = document.querySelector('div.resposta');
-    while (tarefa == ''){
+
+    while (tarefa == '') {
         resp.innerHTML = `O campo está vazio, digite algo`
         inTarefa.focus();
         return;
     }
-  
+
     resp.innerHTML = '';
-    
+
     let lista = document.getElementById('lista');
     let li = document.createElement('li');
     let txt = document.createTextNode(tarefa);
-    
+
     let checkbox = document.createElement('input');
     checkbox.classList.add('checkbox');
     checkbox.type = 'checkbox';
@@ -27,6 +27,27 @@ function addTarefa() {
     inTarefa.value = '';
     inTarefa.focus();
 }
+function excluir() {
+    let checkbox = document.getElementsByTagName('input');
+    let li = document.getElementsByTagName('li');
+    let lista = document.getElementById('lista');
+    let tam = checkbox.length;
+
+
+    var aux = -1;
+
+    for (let i = 0; i < tam; i++) {
+        if (checkbox[i].checked) {
+            aux = i;
+            lista.removeChild(li[0]);
+            break;
+        }
+
+    }
+
+}
+let btExcluir = document.getElementById('btExcluir');
+btExcluir.addEventListener('click', excluir);
 
 
 
