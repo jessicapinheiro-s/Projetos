460bd802ffc822547778e14b8d905699253d53a4
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-06-03 12:20:02 -0300

    ajustando resposta

--- Projeto calculadora IMC/app.js
+++ Projeto calculadora IMC/app.js
@@ -2,12 +2,11 @@ let inAltura = document.getElementById('inAltura');
 let inPeso = document.getElementById('inPeso');
 
 let resp = document.querySelector('div.resposta');
+var resposta = document.getElementById('resposta');
 
 function calcularPeso (){
     let altura = Number(inAltura.value);
     let peso = Number(inPeso.value);
-    
-    var resposta = document.getElementById('resposta');
 
     if(altura == '' || peso == ''){
         resp.innerHTML = `Algum campo está vazio, preencha o campos corretamente ..`
@@ -18,23 +17,26 @@ function calcularPeso (){
         if(arredondado < 18.5){
             resp.innerHTML = `<p>Seu IMC é: ${arredondado}</p> `;
             resp.innerHTML += `classificação: MAGREZA`
-            resposta.style.backgroundColor = '#6f9ff7';
+            resposta.style.backgroundColor = '#ff891c'; //laranja
         }else if(arredondado == 18.5 || arredondado <= 24.9){
             resp.innerHTML = `<p>Seu IMC é: ${arredondado}</p>`;
             resp.innerHTML += `classificação: NORMAL`
-            resposta.style.backgroundColor = '#6ad871';
+            resposta.style.backgroundColor = '#6ad871'; //verde
         }else if(arredondado == 25.0 || arredondado <= 29.9){
             resp.innerHTML = `<p>Seu IMC é: ${arredondado}</p>`;
             resp.innerHTML += `classificação: SOBREPESO`
-            resposta.style.backgroundColor = '#f2e93a';
+            resposta.style.backgroundColor = '#f2e93a'; //amarelo
+            resposta.style.color = '#000';
         }else if(arredondado == 30.0 || arredondado <= 39.9){
             resp.innerHTML = `<p>Seu IMC é: ${arredondado}</p>`;
             resp.innerHTML += `classificação: OBESIDADE`
-            resposta.style.backgroundColor = '#ff891c';
+            resposta.style.backgroundColor = '#ff891c'; //laranja
+            resposta.style.color = '#fff';
         }else if(arredondado > 40.0){
             resp.innerHTML = `<p>Seu IMC é: ${arredondado}</p>`;
             resp.innerHTML += `classificação: OBESIDADE GRAVE`
-            resposta.style.backgroundColor = '#ef3740';
+            resposta.style.backgroundColor = '#ef3740'; //vermelho
+            resposta.style.color = '#fff';
         }
         
     }
@@ -47,7 +49,7 @@ function limpar (){
     let peso = Number(inPeso.value);
 
     resp.innerHTML = '';
-
+    resposta.style.backgroundColor = '#fff'; 
     if(altura == '' || peso == ''){
         resp.innerHTML = `Os campos já estão vazios ..`
     }else{
