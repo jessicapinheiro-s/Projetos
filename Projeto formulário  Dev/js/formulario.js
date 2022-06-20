b641ba375b25354752c50faa1e73facb8a2788cf
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-06-20 16:10:07 -0300

    editando js

--- Projeto formulário  Dev/js/formulario.js
+++ Projeto formulário  Dev/js/formulario.js
@@ -1,25 +1,20 @@
-function enviar() {
+document.getElementById("btEnviar").addEventListener ('click', function(){
     var inNome = document.getElementById("inNome");
     var inSobrenome = document.getElementById("inSobrenome");
     var inNumero = document.getElementById("inNumero");
     var inEmail = document.getElementById("inEmail");
-    var inLinguagem = document.getElementById("inLinguagem");
     var outResposta = document.getElementById("outResposta");
 
     var nome = inNome.value;
-    var sobrenome = inSobrenome.value;
     var numero = Number(inNumero.value);
     var email = inEmail.value;
-    var linguagem = inLinguagem.value;
+
 
     if (numero == '' || nome == '' || email == '' || isNaN(numero)) {
-        alert("Preencha todos os campos..");
+        outResposta.textContent = "Preencha os campos corretamente..";
         inNome.focus();
         return;
     }else{
         outResposta.textContent = "Seu formulário foi enviado com sucesso!";
     }
-
-}
-var btEnviar = document.getElementById("btEnviar");
-btEnviar.addEventListener("click", enviar)
+})
